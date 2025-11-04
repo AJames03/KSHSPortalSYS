b2d3e733ee06a6bc0d81958600910d56faad69b6
AJames03 <betinolaronjames529@gmail.com>
2025-11-04 15:38:47 +0800

    v.2

--- src/app/NewStudent/page.tsx
+++ src/app/NewStudent/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 export const dynamic = 'force-dynamic';
 
-
 import React, { useState, useEffect } from 'react';
 import { Arvo, Poppins } from 'next/font/google';
 import Image from 'next/image';
